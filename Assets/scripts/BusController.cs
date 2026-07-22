406026a785980715f69bfe0a3d6f2c80bb962df8
Isaac r <cowboyjoneswildwest@gmail.com>
2026-07-22 19:10:19 -0400

    updating assets

--- Assets/scripts/BusController.cs
+++ Assets/scripts/BusController.cs
@@ -52,11 +52,6 @@ public class BusController : MonoBehaviour
         rb = GetComponent<Rigidbody>();
     }
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
 
     // Update is called once per frame
     void Update()
